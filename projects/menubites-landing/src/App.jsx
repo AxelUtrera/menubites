7a1c459295cadb3ca24bcf5199f9fce0d35e91f1
Axel Utrera <axeljordanomu@gmail.com>
2024-05-03 00:53:12 -0600

    WIP

--- projects/menubites-landing/src/App.jsx
+++ projects/menubites-landing/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AppLogo from './components/Header.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <AppLogo></AppLogo>
+        <Header></Header>
       </div>
     </>
   )
--- projects/menubites-landing/src/components/Header.jsx
+++ projects/menubites-landing/src/components/Header.jsx
@@ -1,23 +1,90 @@
 import React from 'react';
-import {Box, Center, Container, Flex, Image, Text} from "@chakra-ui/react";
+import {Box, Button, Center, Container, Flex, Grid, GridItem, Text} from "@chakra-ui/react";
 import logoSVG from '../assets/img/Logo.svg'
 
-const AppLogo = () => {
-    return (
-        <Container maxW='90%'>
-            <Box maxWidth='sm'>
-                <Flex>
-                    <img src={logoSVG} alt="MenuBites logo"/>
-                    <Center w='80px'>
-                        <Text fontFamily="Lobster">
-                            MenuBites
-                        </Text>
-                    </Center>
+const homeRoute = '#home'
 
-                </Flex>
-            </Box>
+const Header = () => {
+    return (
+        <Container maxWidth='80%' marginTop='55px' >
+            <Flex>
+                <Logo/>
+                <NavButtons/>
+            </Flex>
         </Container>
     );
 }
 
-export default AppLogo
+
+const NavButtons = () => {
+    return (
+        <Grid templateColumns='repeat(6, 1fr)' maxWidth='100%' marginLeft='140px'>
+            <GridItem colSpan ={1}>
+                <Center height='100%' width='200px'>
+                    <ButtonLink route={homeRoute} text="Home"></ButtonLink>
+                </Center>
+            </GridItem>
+            <GridItem colSpan ={1}>
+                <Center height='100%' width='100px'>
+                    <ButtonLink route={homeRoute} text="About"></ButtonLink>
+                </Center>
+            </GridItem>
+            <GridItem colSpan ={1} width='100px'>
+                <Center height='100%'>
+                    <ButtonLink route={homeRoute} text="Feature"></ButtonLink>
+                </Center>
+            </GridItem>
+            <GridItem colSpan ={1} width='100px'>
+                <Center height='100%'>
+                    <ButtonLink route={homeRoute} text="Contact Us"></ButtonLink>
+                </Center>
+            </GridItem>
+            <GridItem colSpan ={1} width='100px'></GridItem>
+            <GridItem colSpan ={1} width='100px'>
+                <Center height='100%'>
+                    <Button style={{
+                        backgroundImage: 'var(--color-gradient)',
+                        color: '#F3F3F3'
+                    }}>
+                        Join Now
+                    </Button>
+                </Center>
+            </GridItem>
+        </Grid>
+    );
+}
+
+const Logo = () => {
+    return (
+        <Box maxWidth='100%'>
+            <Flex>
+                <img src={logoSVG} alt="MenuBites logo"/>
+                <Center w='80px' marginLeft='10px'>
+                    <Text fontFamily="Lobster" fontSize='20px'>
+                        MenuBites
+                    </Text>
+                </Center>
+            </Flex>
+        </Box>
+    );
+}
+
+const ButtonLink = ({text, route}) => {
+    return (
+        <a href={route} style={{
+            fontFamily: 'Ubuntu',
+            color: '#000000',
+            fontSize: '16px',
+            borderBottom: '2px solid transparent',
+            transition: 'border-bottom 0.3s ease-in-out'
+        }}
+           onMouseOver={(e) => e.target.style.borderBottom = '1px solid currentColor'}
+           onMouseOut={(e) => e.target.style.borderBottom = '1px solid transparent'}
+        >
+            {text}
+        </a>
+    );
+}
+
+
+export default Header
